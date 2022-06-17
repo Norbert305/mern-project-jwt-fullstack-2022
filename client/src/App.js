7ff93a91e5d819ed12e4edb77a6c5e4a996d8920
Norbert305 <sealsnorbert@yahoo.com>
2022-06-17 13:58:20 -0400

    front-end started

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Navbar from './components/Navbar';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+    <div className='container'>
+      <Navbar />
+       <Routes>
+        <Route path="/" element={<Dashboard />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+       </Routes>
+    </div>
+    </Router>
+    </>
+  );
+}
+
+export default App;
